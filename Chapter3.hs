d7725f7f24645600523629f4a7b7138ce6a1f429
ArturKarbone <arturk@diatomenterprises.com>
2014-10-29 17:34:51 +0200

    chapter 3

---
+++ Chapter3.hs
@@ -0,0 +1,13 @@
+﻿--1 + True not all types are compatible
+--the goal of static typing is that compiler will catch
+--these type errors early on, before we run the programm
+
+--:type not (1==1)
+--:type not
+
+--add (x,y) = x + y
+
+--add x y = x +y
+
+-- we define add as a function that takes an integer and returns a function that
+--takes another integer and returns and integer
